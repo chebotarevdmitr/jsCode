1322e0a792b3bdfa88c82602f8f0a5f9f157e02f
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-07-13 23:32:42 +0300

    new file:   dinnamit2.js

---
+++ dinnamit2.js
@@ -0,0 +1,17 @@
+/*
+let a=10;
+a=true;
+a="Dimchasich";
+console.log(a);
+function f(){
+    console.log('Hello, Dimcasic!');
+}
+f();
+*/
+const myCity ={
+    city: "New York",
+}
+myCity.population = 8000000;
+console.log(myCity); 
+delete myCity.population;
+console.log(myCity);
