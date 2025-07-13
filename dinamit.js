b60ac9d19f08dfeabcd5d9f708740190f80c8e24
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-07-13 17:47:48 +0300

    first commit

---
+++ dinamit.js
@@ -0,0 +1,16 @@
+let a = 10;
+console.log(a); // 10
+
+a = "Dimcha";
+console.log(a); // Dimcha
+
+function myFunction() {
+    console.log("Hello from myFunction");
+}
+myFunction(); // Hello from myFunction
+
+const b = () => "Hello Dimcha";
+console.log(b()); // Hello Dimcha
+
+console.log("This is a test script");
+console.log("End of script");
---
+++ testit.js
@@ -0,0 +1,8 @@
+const myName ="Dmitriy";
+const objectA = {a :1,b:2};
+const copyOfA = {objectA};
+copyOfA.a = 20
+console.log(copyOfA)
+copyOfA.c = "abc";
+console.log(copyOfA.c);
+console.log (copyOfA);
