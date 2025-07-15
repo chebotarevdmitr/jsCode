f0504b26107ad2560c43cbd8585b98039ef44748
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-07-15 22:23:40 +0300

    new file:   .vscode/.gitattributes         modified:   .vscode/settings.json         new file:   city.j

---
+++ city.js
@@ -0,0 +1,8 @@
+const myCity = {
+  city: "San Francisco",}
+  myCity["population"] = 883305;
+myCity["area"] = 121.4; // in square kilometers
+console.log(myCity);
+const countryPproertyName = "country";
+myCity[countryPproertyName] = "USA";
+console.log(myCity);
