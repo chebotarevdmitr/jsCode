f43e9b68bfb730fbdb7419c3359e636a3be530fc
Dmitriy Chebotarev <4ebotarov_dim@inbox.ru>
2025-07-16 21:23:58 +0300

    modified:   city.js

--- city.js
+++ city.js
@@ -5,5 +5,17 @@ const myCity ={city:"New York",
 }
 console.log(myCity.info.country);
 console.log(myCity.info.isPopular) // New York
-delete myCity.info["isPopular"];
+delete myCity.info["isPopular"];//скобочна запись
 console.log (myCity)
+const name ="Dmitriy";
+const age = 30;
+const person = {
+    name: name,
+    age: age,
+    city: "Moscow",
+    info: {
+        isStudent: false,
+        hobbies: ["reading", "gaming"]
+    }
+};
+console.log(person); // Dmitriy
